7b76bee67c8128c33f0830214dd1d01e59837222
NintyPercent <NightElfKev@outlook.com>
2016-09-20 23:14:45 +0800

    +添加异常检测练习

---
+++ BasicExceptions/ExceptionPractice/ExceptionPractice/Program.cs
@@ -0,0 +1,72 @@
+﻿/*
+ * 捕获异常
+ * 2016.9.20
+ * NintyPercent
+ * 该程序只是为了实际操作下，并不能正常执行
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass mc = new MyClass();
+            mc.TryExceptionA();
+            mc.TryExceptionB();
+            Console.ReadKey();
+        }
+    }
+
+    //系统检测异常抛出
+    class MyClass
+    {
+        public void TryExceptionA()
+        {
+            int a = 10;
+            int b = 0;
+            try
+            {
+                Console.WriteLine(a / b);
+            }
+            catch(System.IndexOutOfRangeException e)//异常类型不对，不会执行
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("MyClass.TryExceptionA is done");
+            }
+        }
+        /// <summary>
+        /// 主动抛出异常
+        /// </summary>
+        public void TryExceptionB()
+        {
+            int numA = 0;
+            try
+            {
+                if(numA == 0)
+                {
+                    SystemException e = new SystemException();//手动抛出异常，触发Catch
+                    throw (e);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("MyClass.TryExceptionB is Done");
+            }
+        }
+    }
+}
